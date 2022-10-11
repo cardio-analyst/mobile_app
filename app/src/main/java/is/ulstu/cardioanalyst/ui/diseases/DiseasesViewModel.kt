589098936e437e9b1fdea712f01605589dcd45a5
tmrrwnxtsn <tmrrwnxtsn@gmail.com>
2022-10-12 00:40:17 +0400

    Implement the deseases

--- app/src/main/java/is/ulstu/cardioanalyst/ui/diseases/DiseasesViewModel.kt
+++ app/src/main/java/is/ulstu/cardioanalyst/ui/diseases/DiseasesViewModel.kt
@@ -1,10 +1,21 @@
 package `is`.ulstu.cardioanalyst.ui.diseases
 
-import `is`.ulstu.foundation.navigator.Navigator
+import `is`.ulstu.cardioanalyst.App
+import `is`.ulstu.cardioanalyst.R
+import `is`.ulstu.cardioanalyst.models.diseases.IDiseasesRepository
+import `is`.ulstu.cardioanalyst.models.users.IUserRepository
+import `is`.ulstu.foundation.uiactions.UiActions
 import `is`.ulstu.foundation.views.BaseViewModel
 
 class DiseasesViewModel(
-    private val navigator: Navigator,
+    private val uiActions: UiActions,
+    private val userRepository: IUserRepository,
+    private val diseasesRepository: IDiseasesRepository
 ) : BaseViewModel() {
+    fun getUserDiseases() = diseasesRepository.getUserDiseases(userRepository.getCurrentUserToken())
 
+    fun setUserDiseases(diseases: Map<String, Boolean>) {
+        diseasesRepository.setUserDiseases(userRepository.getCurrentUserToken(), diseases)
+        uiActions.toast(App.appResources.getString(R.string.user_info_save))
+    }
 }
