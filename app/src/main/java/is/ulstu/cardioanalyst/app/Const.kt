7dc4bf323ff5eb85e6bcc383959258e11c276893
Orthodoxal <dimariktron@mail.ru>
2023-03-15 12:08:28 +0400

    Gradle multi module prepare

--- app/src/main/java/is/ulstu/cardioanalyst/app/Const.kt
+++ app/src/main/java/is/ulstu/cardioanalyst/app/Const.kt
@@ -2,9 +2,9 @@ package `is`.ulstu.cardioanalyst.app
 
 object Const {
     // standard URL for local docker
-    // const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
     // for network
-    const val BASE_URL = "https://cardio-analyst-server.onrender.com/api/v1/"
+    //const val BASE_URL = "https://cardio-analyst-server.onrender.com/api/v1/"
     const val BEARER_AUTH = "Bearer "
 
     const val REGEX_EMAIL =
