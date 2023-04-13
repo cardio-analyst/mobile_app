55330dc19c869bcce5853de7eb609c575bf9799a
Orthodoxal <dimariktron@mail.ru>
2023-04-13 23:08:17 +0400

    Theme correcting

--- data/src/main/java/com/example/data/Const.kt
+++ data/src/main/java/com/example/data/Const.kt
@@ -2,8 +2,8 @@ package com.example.data
 
 object Const {
     // standard URL for local docker
-    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    // const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
     // for network
-    // const val BASE_URL = "https://cardio-analyst-server.onrender.com/api/v1/"
+    const val BASE_URL = "https://cardio-analyst-server.onrender.com/api/v1/"
     const val BEARER_AUTH = "Bearer "
 }
